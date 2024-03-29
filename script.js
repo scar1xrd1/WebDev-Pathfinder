06fb37e4e5b97e053890e9de8ea6fa1b2721d722
scar1xrd <kotlaranton551@gmail.com>
2024-03-29 18:04:19 +0200

    added welcome page

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var isImage = false
 
+$('#authorized').hide()
+
+$('#login-button').hover(function () {
+    $(this).find('.fa-regular').removeClass('fa-regular').addClass('fa-solid')
+}, function () {
+    $(this).find('.fa-solid').removeClass('fa-solid').addClass('fa-regular')
+})
+$('#reg-button').hover(function () {
+    $(this).find('.fa-regular').removeClass('fa-regular').addClass('fa-solid')
+}, function () {
+    $(this).find('.fa-solid').removeClass('fa-solid').addClass('fa-regular')
+})
+
 $('document').ready(function () {
-    $('#btest').click(function () {
-        if (!isImage) {
-            $('body').css('background-image', 'url(images/t1.jpg)')
-            isImage = true
-        }
-        else {
-            $('body').css('background-image', 'none')
-            isImage = false
-        }
-    })
+
 })
